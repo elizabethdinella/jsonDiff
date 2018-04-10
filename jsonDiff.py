fed4b2f6d796affe8deb9bfdaa6af5c9e8786068
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2018-04-10 14:52:10 -0400

    Added context dependent equality

--- jsonDiff.py
+++ jsonDiff.py
@@ -13,11 +13,24 @@ TODO: change this equality map to allow for more than two languages
 	The flow is that we check for tag equality. If there is not tag equality, then we check for structural equality, mark the node, and move	 on
 '''
 
+class Context:
+	def __init__(self, parentTags):
+		self.parentTags = parentTags
+
+	def __eq__(self, other):
+		for tag in self.parentTags:
+			for tag_ in other.parentTags:
+				if tag.lower() == tag_.lower():
+					return True
+		return False
+
+#NOTE: everything in these maps must be ALL lowercase
 
 #Context independent equality - (Tag1 always == Tag2 and Tag2 always == Tag1 regardless of context)
-tagEqualityMap = dict({"ClassDef": "class", "FunctionDef": "function", "CompoundStmt": "body"})
+tagEqualityMap = dict({"classdef": "class", "functiondef": "function", "compoundstmt": "body"})
 
 #context dependent equality - (Tag1 always == Tag2 in the context A)
+tagEqlMap2 = dict({"case":("compoundstmt", Context(["if"])), "else":("compoundstmt", Context(["if"]))})
 
 #Context dependent - if this parent->child sub-tree is found, skip the child node. It is an extra node in one of the trees
 structEqlMap = dict({"class":"body"})
@@ -61,59 +74,54 @@ def inStructEqlMap(parent, node):
 	return False
 
 
-#def structuralEquality(node):''', node2'''
+'''
+Wrapper around inStructEqlMap
+'''
 def structuralEquality(node):
-
-
-	#if not "parent" in node: '''or not "parent" in node2'''
-	if not "parent" in node:
+	if not "parent" in node or not "tags" in node:
 		return False
 
-	#for the print 
-	#if not "tags" in node: '''or not "tags" in node2'''
-	if not "tags" in node: 
-		return False
+	return inStructEqlMap(node["parent"], node)
 
-	node1Str = False
-	node2Str = False
-	if inStructEqlMap(node["parent"], node):
-		node1Str = True
-		#elif inStructEqlMap(node2["parent"], node2):
-		#node2Str = True'
-	else:
-		return False
-
-	'''
-	if tagsMatch(node["parent"], node2["parent"]):
-		if node1Str: node["matched"] = True
-		else: node2["matched"] = True
-		
-		return True
-
-	return False	
-	'''
-
-	return True
-
-
+'''
+Checks if two tags are equal using the equality map
+'''
 def equalTags(tag, tag_):
 	if tag.lower() == tag_.lower():
 		return True
-	elif (tag in tagEqualityMap and tagEqualityMap[tag] == tag_):
+	elif (tag.lower() in tagEqualityMap and tagEqualityMap[tag.lower()] == tag_.lower()):
 		return True
-	elif (tag_ in tagEqualityMap and tagEqualityMap[tag_] == tag):
+	elif (tag_.lower() in tagEqualityMap and tagEqualityMap[tag_.lower()] == tag.lower()):
 		return True
 	else:
 		return False
-			
 
-def tagsMatch(tags1, tags2):
+
+def equalContextTags(tag, tag_, parent1Tags, parent2Tags):
+	if (tag.lower() in tagEqlMap2 and tagEqlMap2[tag.lower()][0] == tag_.lower() 
+		and tagEqlMap2[tag.lower()][1] == Context(parent1Tags)):
+			return True
+
+	if (tag_.lower() in tagEqlMap2 and tagEqlMap2[tag_.lower()][0] == tag.lower()
+		and tagEqlMap2[tag_.lower()][1] == Context(parent2Tags)):
+			return True
+
+	return False
+			
+'''
+Given two nodes, check if any tags are equal
+'''
+def tagsMatch(tags1, tags2, parent1Tags, parent2Tags):
 	for tag in tags1:
 		for tag_ in tags2:
 			if equalTags(tag, tag_ ):
 				return True
-
-	return False;
+	for tag in tags1:
+		for tag_ in tags2:
+			if equalContextTags(tag, tag_, parent1Tags, parent2Tags):
+				print("equal context!")
+				return True
+	return False
 
 def hasTags(node):
 	return "tags" in node 
@@ -136,6 +144,12 @@ def delAddedTags(node):
 	
 	for child in node["children"]:
 		delAddedTags(child)
+
+def nodesMatch(node, node2, parent, parent2):
+	return ((hasTags(node) and hasTags(node2) and 
+		tagsMatch(node["tags"], node2["tags"], parent["tags"], parent2["tags"]) and not node2["matched"]) 
+		or (not hasTags(node) and not hasTags(node2) and typesMatch(node, node2) and datasMatch(node, node2)))
+
 
 '''Main recursive function - Three recursions occur:
 (1) Recurses down the tree to find nodes with context independent tag matching
@@ -162,9 +176,7 @@ def checkChildren(t1Nodes, t2Nodes, parent1, parent2):
 	'''
 	for node in t1Nodes:	
 		for node2 in t2Nodes:
-			if((hasTags(node) and hasTags(node2) and tagsMatch(node["tags"], node2["tags"]) and not node2["matched"]) or 
-				(not hasTags(node) and not hasTags(node2) and typesMatch(node, node2) and datasMatch(node, node2))):
-
+			if nodesMatch(node, node2, parent1, parent2):		
 				node["matched"] = True
 				node2["matched"] = True
 
@@ -177,62 +189,42 @@ def checkChildren(t1Nodes, t2Nodes, parent1, parent2):
 	Second recurion - On the way up, check for structural equality 
 			(a node level that exists in one tree but not the other)
 
-	'''
-
-	for node in t1Nodes:
-		strEql = False
-		if not node["matched"]:
-			strEql = False
-			if structuralEquality(node):
-				markNode(node, True)
-				strEql = True
-				print("struct eql")
-				checkChildren(node["children"], node2["parent"]["children"], node, node2["parent"])
-
-			if not strEql: markSubtree(node, True)
-		else:
-			grayCount1+=1
-		
-
-	'''
-	for node in t1Nodes:
-		if not node["matched"]:
-			strEql = False
-			for node2 in t2Nodes:
-				if not node2["matched"] and structuralEquality(node, node2):
-					markNode(node, True)
-					strEql = True
-					print("struct eql")
-
-					#if "children" in node and "children" in node2: 
-					if node["matched"]:
-						checkChildren(node["children"], node2["parent"]["children"], node, node2["parent"])
-					break
-					#if there is structual equality then we need to skip a level 
-				
-			if not strEql: markSubtree(node, True)
-		else:
-			grayCount1 += 1
-	'''
-			
 	
-	for node in t2Nodes:
-		if not node["matched"]:
-			strEql = False
-			for node2 in t1Nodes:
-				'''
-				if not node2["matched"] and structuralEquality(node, node2):
-					markNode(node, False)
-					strEql = True
-					print("struct eql")
-					break
-				'''
-
-			if not strEql: markSubtree(node, False)
-		else:
-			grayCount2 += 1
+	'''
+	unmatchedNodes = (node for node in t1Nodes if not node["matched"])
+	for node in unmatchedNodes:
+		if structuralEquality(node):
+			node["matched"] = True
+			markNode(node, True)
+			print("struct eql")
+			#recurse with the unmatched node, and a tree2 node from the current level
+			checkChildren(node["children"], node2["parent"]["children"], node, node2["parent"])
+		#else: markSubtree(node, True)
 
 
+
+
+	unmatchedNodes = (node2 for node2 in t2Nodes if not node2["matched"])
+	for node2 in unmatchedNodes:
+		if structuralEquality(node2):
+			node2["matched"] = True
+			markNode(node2, False)
+			print("struct eql")
+			#recurse with the numatched node, and a tree1 node from the current level
+			checkChildren(node["parent"]["children"], node2["children"], node["parent"], node2)
+		#else: markSubtree(node2, False)
+
+
+	unmatchedNodes = (node for node in t1Nodes if not node["matched"])
+	for node in unmatchedNodes:
+		markSubtree(node, True)		
+
+	unmatchedNodes = (node2 for node2 in t2Nodes if not node2["matched"])
+	for node2 in unmatchedNodes:
+		markSubtree(node2, False)	
+
+
+	#grayCount1+=1 for all matchedNodes
 
 def runner():
 	if len(sys.argv) < 3:
@@ -247,7 +239,7 @@ def runner():
 
 	if not hasTags(jsonObj1) or not hasTags(jsonObj2):
 		print("error: illformatted json doesn't have tags")
-	elif not tagsMatch(jsonObj1["tags"], jsonObj2["tags"]):
+	elif not tagsMatch(jsonObj1["tags"], jsonObj2["tags"], None, None):
 		print("error: root nodes don't match")
 	else:
 		checkChildren(jsonObj1["children"], jsonObj2["children"], jsonObj1, jsonObj2)
